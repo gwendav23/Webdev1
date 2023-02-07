557c38ff05f8d882df5c5a77a7d51640b8600b73
gwendav23 <122967216+gwendav23@users.noreply.github.com>
2023-02-07 22:03:41 +0100

    version 2: Locations display version 2 + page locations details

--- src/routes/locations/+page.svelte
+++ src/routes/locations/+page.svelte
@@ -1,6 +1,21 @@
 <script>
+	import { enhance } from '$app/forms';
+	import ListErrors from '$lib/ListErrors.svelte';
 	/** @type {import('./$types').PageData} */
 	export let data;
+	export let form;
+	let message = "Hello from parent";
+	function handleClick(id,filmName,filmType,filmProducerName,filmDirectorName,address,year) {
+		const params = new URLSearchParams();
+		params.set('id', id);
+		params.set('filmName', filmName);
+		params.set('filmType', filmType);
+		params.set('filmProducerName', filmProducerName);
+		params.set('filmDirectorName', filmDirectorName);
+		params.set('address', address);
+		params.set('year', year);
+		window.location.href = `/locationsDetails?${params.toString()}`;
+	}
 </script>
 
 <svelte:head>
@@ -26,6 +41,8 @@
 								<tr>
 									<th>{location.filmName}</th>
 									<th>{location.filmProducerName}</th>
+									<button on:click={() => handleClick(location._id,location.filmName,location.filmType,location.filmProducerName,location.filmDirectorName,location.address, location.year)}> Détails </button>
+
 								</tr>
 							{/each}
 						</table>
@@ -35,6 +52,56 @@
 		</div>
 	</div>
 </div>
+
+<style>
+	h1{
+		text-align: center;
+		padding: 10px;
+		width: auto;
+		height: 120px;
+	}
+	button{
+		display: inline-block;
+		background-color: #381506 ;
+		border-radius: 18px;
+		border: 4px  #cccccc;
+		color: #eeeeee;
+		text-align: center;
+		font-size: 13px;
+		padding: 20px;
+		width: 100px;
+		transition: all 0.5s;
+		cursor: context-menu;
+		margin: 10px;
+	}
+	button:hover {
+		background-color: green;
+	}
+	body{
+		background-color: #f2d689;
+		z-index: 1;
+	}
+	#titre_column th {
+		text-align: center;
+		background-color: #D4EFDF;
+		border-spacing: 25px;
+		font-weight: 900;
+		border-style: groove;
+	}
+	#contenu th{
+		border: 9px  ;
+		font-weight: 300 ;
+		color: #4D5656;
+		padding: 8px;
+		text-align: left;
+	}
+	table {
+		width: 100%;
+	}
+	th {
+		background-color: #E7F7EF;
+	}
+</style>
 
 
 
---
+++ src/routes/locationsDetails/+page.svelte
@@ -0,0 +1,93 @@
+<script>
+	import {onMount} from "svelte";
+	/** @type {import('./$types').PageData} */
+	export let message;
+	export let id;
+	export let filmName;
+	export let filmType;
+	export let filmProducerName;
+	export let filmDirectorName;
+	export let address;
+	export let year;
+	onMount(async () => {
+		const params = new URLSearchParams(window.location.search);
+		id = params.get('id');
+		filmName = params.get("filmName");
+		filmType = params.get('filmType');
+		filmProducerName = params.get('filmProducerName');
+		filmDirectorName = params.get('filmDirectorName');
+		address = params.get('address');
+		year = params.get('year');
+	});
+</script>
+
+<svelte:head>
+	<title> Détails • Conduit</title>
+</svelte:head>
+
+<body>
+<div class="auth-page">
+	<div class="container page">
+		<div class="row">
+			<div class="col-md-6 offset-md-3 col-xs-12">
+				<h1 class="text-xs-center">Details</h1>
+				<div>
+					<table>
+						<tr>
+							<th>id</th>
+							<th>filmName</th>
+							<th>filmType</th>
+							<th>filmProducerName</th>
+							<th>filmDirectorName</th>
+							<th>address</th>
+							<th>year</th>
+
+						</tr>
+						<tr>
+							<td>{id}</td>
+							<td>{filmName}</td>
+							<td>{filmType}</td>
+							<td>{filmProducerName}</td>
+							<td>{filmDirectorName}</td>
+							<td>{address}</td>
+							<td>{year}</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+<style>
+	h1{
+		text-align: center;
+		padding: 18px;
+		width: auto;
+		height: 137px;
+		margin-left: 26REM;
+	}
+	table {
+		margin: auto;
+	}
+	th {
+		text-align: center;
+		background-color: #D4EFDF;
+		border-spacing: 25px;
+		font-weight: 900;
+		border-style: groove;
+		padding: 20px;
+		margin: 30px;
+	}
+	.row {
+		margin-right: 50rem;
+	}
+	body {
+		background-color: #f2d689;
+	}
+	td {
+		background-color: #E9F7EF;
+		padding-right: 20px;
+		padding-left: 30px;
+	}
+</style>
