7ffd37cce4aa96ab4f9e4e78530f9d26f08f7a13
gwendav23 <122967216+gwendav23@users.noreply.github.com>
2023-02-12 01:04:55 +0100

    version 3: Read me Update

--- README.md
+++ README.md
@@ -4,9 +4,10 @@
 
 ### [Demo](https://realworld.svelte.dev)&nbsp;&nbsp;&nbsp;&nbsp;[RealWorld](https://github.com/gothinkster/realworld)
 
-This codebase was created to demonstrate a fully fledged fullstack application built with SvelteKit including CRUD operations, authentication, routing, pagination, and more.
-
-For more information on how to this works with other frontends/backends, head over to the [RealWorld](https://github.com/gothinkster/realworld) repo.
+This code use teh application real word but with some modification since we choose to display some movies location and other details. We used a back end created by our tutor. 
+we worked within the locations, locationDetails login and register files. The first step was to connect the back and the frontend. In teh backend we create a .env file where we define 
+a jwt which is the token that will allow the authentication besides we add teh JSON file. Then, while the app is launch the user has to log in or register to access to the movies locations.
+he cans use the more info button to get more details about the movie. 
 
 ## Running locally
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,77 +1,56 @@
 <script>
 	import { page } from '$app/stores';
-	import ArticleList from '$lib/ArticleList/index.svelte';
-	import Pagination from './Pagination.svelte';
-
-	/** @type {import('./$types').PageData} */
-	export let data;
-
-	$: p = +($page.url.searchParams.get('page') ?? '1');
-	$: tag = $page.url.searchParams.get('tag');
-	$: tab = $page.url.searchParams.get('tab') ?? 'all';
-	$: page_link_base = tag ? `tag=${tag}` : `tab=${tab}`;
 </script>
 
 <svelte:head>
-	<title>Conduit</title>
+	<title>{$page.status}</title>
 </svelte:head>
 
-<div class="home-page">
-	{#if !data.user}
-		<div class="banner">
-			<div class="container">
-				<h1 class="logo-font">conduit</h1>
-				<p>A place to share your knowledge.</p>
-			</div>
-		</div>
+<div>
+	{#if $page.status === 404}
+		<h1> Welcome,
+			<button onclick="window.location.href = '/locations';">Movie locations</button>
+		</h1>
+
+	{:else}
+		<h1>Something went wrong</h1>
 	{/if}
-
-	<div class="container page">
-		<div class="row">
-			<div class="col-md-9">
-				<div class="feed-toggle">
-					<ul class="nav nav-pills outline-active">
-						<li class="nav-item">
-							<a href="/?tab=all" class="nav-link" class:active={tab === 'all' && !tag}>
-								Global Feed
-							</a>
-						</li>
-
-						{#if data.user}
-							<li class="nav-item">
-								<a href="/?tab=feed" class="nav-link" class:active={tab === 'feed'}>Your Feed</a>
-							</li>
-						{:else}
-							<li class="nav-item">
-								<a href="/login" class="nav-link">Sign in to see your Feed</a>
-							</li>
-						{/if}
-
-						{#if tag}
-							<li class="nav-item">
-								<a href="/?tag={tag}" class="nav-link active">
-									<i class="ion-pound" />
-									{tag}
-								</a>
-							</li>
-						{/if}
-					</ul>
-				</div>
-
-				<ArticleList articles={data.articles} />
-				<Pagination pages={data.pages} {p} href={(p) => `/?${page_link_base}&page=${p}`} />
-			</div>
-
-			<div class="col-md-3">
-				<div class="sidebar">
-					<p>Popular Tags</p>
-					<div class="tag-list">
-						{#each data.tags as tag}
-							<a href="/?tag={tag}" class="tag-default tag-pill">{tag}</a>
-						{/each}
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
 </div>
+
+<style>
+	h1 {
+		margin: 3.5em 0;
+		text-align: center;
+	}
+	body{
+		background-color: #f2d689;
+		z-index: 1;
+	}
+	h1 {
+		text-shadow: 2px 1px #381506 ;
+		color: #12251d;
+	}
+
+	button{
+		display: inline-block;
+		background-color: #381506;
+		border-radius: 10px;
+		border: 4px  #cccccc;
+		color: #eeeeee;
+		text-align: center;
+		font-size: 16px;
+		width: 400px;
+		transition: all 0.5s;
+		cursor: context-menu;
+		margin: 65px;
+		padding: 15px;
+	}
+	button:hover {
+		background-color: green;
+	}
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
