30ac5d0b8e8869ce95cb5069d4f6ef2e6661c6ff
gwendav23 <122967216+gwendav23@users.noreply.github.com>
2023-02-17 17:52:26 +0100

    version 4: To deploy

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -2,10 +2,7 @@
 export function load({ locals }) {
 	return {
 		user: locals.user && {
-			username: locals.user.username,
-			email: locals.user.email,
-			image: locals.user.image,
-			bio: locals.user.bio
+			username: locals.user.username
 		}
 	};
 }
